991842814127d1e1f586ea72cb0cec8c2b921c98
TAMIRIS REZENDE GARCIA COSTA <tamirisrgarcia@gmail.com>
2023-05-11 21:37:32 -0300

    feat: criacao do component footer responsivo

--- FrontEnd/projeto/projetoIntegrador/src/App.jsx
+++ FrontEnd/projeto/projetoIntegrador/src/App.jsx
@@ -1,8 +1,12 @@
+import Footer from '../src/components/Footer'
+import './global.css';
+
 function App() {
 
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Footer />
     </div>
   )
 }
