24005e31f12340eaaec60efba98e3234cb0756dc
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-05-16 22:20:16 -0300

    Mexendo na classe Categoria

--- backEnd/src/main/java/com/airbnb/model/Categoria.java
+++ backEnd/src/main/java/com/airbnb/model/Categoria.java
@@ -4,12 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.ToString;
 
-@Getter
-@Setter
 @ToString
-@NoArgsConstructor
 @Entity
 public class Categoria
 {
@@ -19,6 +16,8 @@ public class Categoria
 	private double qualificacao;
 	private String descricao, urlImagem;
 	
+	public Categoria() {}
+	
 	public Categoria(double qualificacao, String descricao, String urlImagem)
 	{
 		this.qualificacao = qualificacao;
@@ -26,4 +25,35 @@ public class Categoria
 		this.urlImagem = urlImagem;
 	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public double getQualificacao() {
+		return qualificacao;
+	}
+
+	public void setQualificacao(double qualificacao) {
+		this.qualificacao = qualificacao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
 }
