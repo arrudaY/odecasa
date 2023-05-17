0199ae77cad7dfd6da6e125280274878b4c51cfd
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-05-17 20:13:03 -0300

    Consumo da API de Categorias

--- FrontEnd/projeto/projetoIntegrador/src/Components/Categorias/Categorias.jsx
+++ FrontEnd/projeto/projetoIntegrador/src/Components/Categorias/Categorias.jsx
@@ -3,33 +3,42 @@ import img01 from "../../Data/no_image.png";
 import img02 from "../../Data/no_image.png";
 import img03 from "../../Data/no_image.png";
 import img04 from "../../Data/no_image.png";
+import api from "../../Services/api";
+import { useEffect , useState} from "react";
 
 const Categorias = () => {
+    const [categorias, setCategorias] = useState([]);
+
+    async function getCategorias(){
+        try {
+          const response = await api.get("/categoria",
+          { headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          }});
+          console.log(response.data);
+          setCategorias(response.data);
+        } catch (error) {
+          console.log(error)
+        }
+    }
+    
+    useEffect(() => {
+        getCategorias();    
+    }, []);
 
   return (
     <div className={styles.categoriasContainer}>
         <h2>Buscar por tipo de acomodação</h2>
         <div className={styles.categoriasCards}>
-            <div className={styles.categoriasCard}>
-                <img className={styles.categoriasImg} src={img01}/>
-                <h2>Hotéis e Hostels</h2>
-                <p>X acomodações</p>
-            </div>
-            <div className={styles.categoriasCard}>
-                <img className={styles.categoriasImg} src={img02}/>
-                <h2>Apartamentos inteiros</h2>
-                <p>X acomodações</p>
-            </div>
-            <div className={styles.categoriasCard}>
-                <img className={styles.categoriasImg} src={img03}/>
-                <h2>Quartos</h2>
-                <p>X acomodações</p>
-            </div>
-            <div className={styles.categoriasCard}>
-                <img className={styles.categoriasImg} src={img04}/>
-                <h2>Pousadas</h2>
-                <p>X acomodações</p>
-            </div>
+            {categorias.map((item) => (
+                <div key={item.id} className={styles.categoriasCard}>
+                    <img className={styles.categoriasImg} src={item.urlImagem}/>
+                    <h2>{item.descricao}</h2>
+                    <p>X acomodações</p>
+                </div>
+            ))}
         </div>
     </div>
   );
