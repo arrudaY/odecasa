cd42e10a1be3d5f5120feec7c8ee09d9ccb113e3
Vitor Mateus <vic.matteus@alu.ufc.br>
2023-05-27 15:34:40 -0300

    exclusão de import

--- backEnd/src/main/java/com/airbnb/service/ProdutoService.java
+++ backEnd/src/main/java/com/airbnb/service/ProdutoService.java
@@ -5,7 +5,6 @@ import com.airbnb.model.Produto;
 import com.airbnb.repository.ProdutoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
