c142b799ce4ae8a04bfd3c2ff7426af6e2116ec1
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-05-31 22:18:39 -0300

    Finalizando Reserva na pagina do produto

--- FrontEnd/projeto/projetoIntegrador/src/Components/ReservaProd/ReservaProd.jsx
+++ FrontEnd/projeto/projetoIntegrador/src/Components/ReservaProd/ReservaProd.jsx
@@ -1,13 +1,19 @@
 import styles from "./ReservaProd.module.css";
 import { useContext } from "react";
 import { ProdContext } from "../../Contexts/ProdContext";
+import { DateRangePicker } from "rsuite";
 
 const ReservaProd = () => {    
     const { id, produto } = useContext(ProdContext);
 
     return (
-        <div className={styles.reservaContainer}>
-            Hello World Reserva - ID: {id}
+        <div className={styles.reservaContainer}>            
+            <h2>Escolha as suas datas</h2>
+            <div className="divisoria"></div>
+            <div className={styles.reservaReserva}>
+                <DateRangePicker size="lg" placeholder="Checkin ~ Checkout" format={"dd-MM-yyyy"} className={styles.reservaTxt} />
+                <button className={styles.reservaBtn}>Fazer Reserva</button>
+            </div>
         </div>
     );
 };
