ff7ef341220b56eeb54acf07bf807d90cb43dc66
Vitor Mateus <vic.matteus@alu.ufc.br>
2023-06-02 21:35:00 -0300

    atualizacao fim da sprint 2

--- backEnd/src/main/java/com/airbnb/model/Categoria.java
+++ backEnd/src/main/java/com/airbnb/model/Categoria.java
@@ -1,12 +1,10 @@
 package com.airbnb.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name="categorias")
 public class Categoria
 {
 	@Id
