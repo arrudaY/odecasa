aff3273547413f3cd4aadf231d33ab871c90c1d2
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-06-05 20:27:10 -0300

    Ajeitando lista de imagens com menos de 5

--- FrontEnd/projeto/projetoIntegrador/src/Components/ImgProd/ImgProd.jsx
+++ FrontEnd/projeto/projetoIntegrador/src/Components/ImgProd/ImgProd.jsx
@@ -1,14 +1,11 @@
 import styles from "./ImgProd.module.css";
-import { useContext } from "react";
-import { ProdContext } from "../../Contexts/ProdContext";
 import  GaleriaProdutos from "../GaleriaProdutos/GaleriaProdutos";
 
 const ImgProd = () => {    
-    const { id, produto } = useContext(ProdContext);
 
     return (
         <div className={styles.imgContainer}>
-            <GaleriaProdutos id={id} produto={produto} />
+            <GaleriaProdutos />
         </div>
     );
 };
