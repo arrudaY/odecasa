eb4d218e26e7c6e5aac87042630b397fcff17a20
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-06-06 20:06:28 -0300

    Colocando API do servidor da AWS para testes

--- FrontEnd/projeto/projetoIntegrador/src/Services/api.js
+++ FrontEnd/projeto/projetoIntegrador/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://ec2-54-226-11-155.compute-1.amazonaws.com:8080",
 });
 
 export default api;
