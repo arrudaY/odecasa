71efb530a8e10beb2ac450c417dc44665972c582
Vitor Mateus <vic.matteus@alu.ufc.br>
2023-06-13 23:18:03 -0300

    adição dos cross-origin

--- backEnd/src/main/java/com/airbnb/controller/AuthController.java
+++ backEnd/src/main/java/com/airbnb/controller/AuthController.java
@@ -10,13 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping(value = "/auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthController
 {
 	@Autowired
--- backEnd/src/main/java/com/airbnb/controller/ReservaController.java
+++ backEnd/src/main/java/com/airbnb/controller/ReservaController.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/reserva")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ReservaController
 {
 	@Autowired
