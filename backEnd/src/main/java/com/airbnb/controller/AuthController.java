32493777e04d18c885f14130ca96bb54c4ed1ca1
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-06-16 20:07:32 -0300

    Resolução do problema de CORS

--- backEnd/src/main/java/com/airbnb/controller/AuthController.java
+++ backEnd/src/main/java/com/airbnb/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 @RequestMapping(value = "/auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthController
 {
 	@Autowired
