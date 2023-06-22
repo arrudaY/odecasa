f40c4af3517927b49bd071354088d893833ecaef
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-06-22 20:04:17 -0300

    Ajustando as avaliações dos produtos nos cards da Home

--- FrontEnd/projeto/projetoIntegrador/src/Components/CardBusca/CardBusca.jsx
+++ FrontEnd/projeto/projetoIntegrador/src/Components/CardBusca/CardBusca.jsx
@@ -10,6 +10,19 @@ const CardBusca = (props) => {
     navigate(`/detalhes/${props.produto.id}`);
   }
 
+  function verificarAvalicao(){
+    if((props.produto.qualificacao > 4.0) && (props.produto.qualificacao <= 5.0))
+        return "Maravilhoso";
+    else if((props.produto.qualificacao > 3.0) && (props.produto.qualificacao <= 4.0))
+        return "Muito bom";
+    else if((props.produto.qualificacao > 2.0) && (props.produto.qualificacao <= 3.0))
+        return "Razoável";
+    else if((props.produto.qualificacao > 1.0) && (props.produto.qualificacao <= 2.0))
+        return "Regular";
+    else if(props.produto.qualificacao <= 1.0)
+        return "Ruim";
+}
+
   return (
 
     <div className={styles.cardBuscaContainer}>
@@ -27,18 +40,18 @@ const CardBusca = (props) => {
             <div className={styles.cardAvaliacao}>
 
               <div className={styles.cardClassificacao}>
-                <span>Excelente</span>
-                <div className={styles.cardStarsRate}>
-                  <StarRateIcon sx={{ fontSize: 12 }}/>
-                  <StarRateIcon sx={{ fontSize: 12 }}/>
-                  <StarRateIcon sx={{ fontSize: 12 }}/>
-                  <StarRateIcon sx={{ fontSize: 12 }}/>
-                  <StarRateIcon sx={{ fontSize: 12 }}/>
+                <p>{verificarAvalicao()}</p>
+                <div className="stars"> 
+                  <span className={`star ${props.produto.qualificacao >= 1.0 ? "on" : props.produto.qualificacao == 0.0 ? "" : "half"}`}></span>
+                  <span className={`star ${props.produto.qualificacao >= 2.0 ? "on" : props.produto.qualificacao <= 1.0 ? "" : "half"}`}></span>
+                  <span className={`star ${props.produto.qualificacao >= 3.0 ? "on" : props.produto.qualificacao <= 2.0 ? "" : "half"}`}></span>
+                  <span className={`star ${props.produto.qualificacao >= 4.0 ? "on" : props.produto.qualificacao <= 3.0 ? "" : "half"}`}></span>
+                  <span className={`star ${props.produto.qualificacao == 5.0 ? "on" : props.produto.qualificacao <= 4.0 ? "" : "half"}`}></span>
                 </div>
               </div>
 
               <div className={styles.cardNota}>
-                <span>9.0</span>
+                <span>{(props.produto.qualificacao * 2.0).toFixed(1)}</span>
               </div>
             </div>
           </div>
