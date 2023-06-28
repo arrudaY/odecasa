572642f7eb890690f8e20bf345ea82f3c048f403
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-06-28 18:05:30 -0300

    Layout esqueleto da tela de cadastrar produto pronto

--- FrontEnd/projeto/projetoIntegrador/src/Components/FormProd/FormProd.jsx
+++ FrontEnd/projeto/projetoIntegrador/src/Components/FormProd/FormProd.jsx
@@ -4,6 +4,7 @@ import { FormContext } from "../../Contexts/FormContext";
 import imgVoltar from "../../Data/back.png"
 import { useNavigate } from "react-router-dom";
 import addImg from "../../Data/add.png"
+import uploadImg from "../../Data/upload.png"
 
 const FormProd = () => {
     const { categorias, setCategorias, cidades, setCidades, isLoading, setIsLoading } = useContext(FormContext);
@@ -49,11 +50,11 @@ const FormProd = () => {
                     </div>
                     <div className={styles.blocaoTxtBasicoDesc}>
                         <label className={styles.formLabel}>Descrição curta</label>
-                        <input type="text" className={styles.formTxtDesc}/> 
+                        <textarea className={styles.formTxtDesc}/> 
                     </div>
                     <div className={styles.blocaoTxtBasicoDesc}>
                         <label className={styles.formLabel}>Descrição longa</label>
-                        <input type="text" className={styles.formTxtDesc}/> 
+                        <textarea className={styles.formTxtDesc}/> 
                     </div>
                     <h3>Adicionar atributos</h3>
                     <div className={styles.formAtributos}>
@@ -68,6 +69,46 @@ const FormProd = () => {
                             </div>
                         </div>
                         <img className={styles.formAddImg} src={addImg}></img>
+                    </div>
+                    <h3>Políticas da Acomodação</h3>
+                    <div className={styles.formPoliticas}>
+                        <div className={styles.formPoliticasBloco}>
+                            <h5>Regras da acomodação</h5>
+                            <label className={styles.formLabel}>Checkin</label>
+                            <input type="text" className={styles.formTxt}/> 
+                            <label className={styles.formLabel}>Checkout</label>
+                            <input type="text" className={styles.formTxt}/> 
+                            <label className={styles.formLabel}>Outras regras</label>
+                            <textarea className={styles.formTxtDesc}/> 
+                        </div>
+                        <div className={styles.formPoliticasBloco}>
+                            <h5>Saúde e Segurança</h5>
+                            <label className={styles.formLabel}>Diretrizes da acomodação</label>
+                            <textarea className={styles.formTxtDesc}/> 
+                        </div>
+                        <div className={styles.formPoliticasBloco}>
+                            <h5>Políticas de Cancelamento</h5>
+                            <label className={styles.formLabel}>Políticas da acomodação</label>
+                            <textarea className={styles.formTxtDesc}/> 
+                        </div>
+                    </div>
+                    <h3>Carregar Imagens</h3>
+                    <div className={styles.formAtributos}>
+                        <h5>Imagem Principal</h5>
+                        <div className={styles.formCarregarImg}>
+                            <input type="text" className={styles.formCarregarImgTxt}/> 
+                            <img className={styles.formUploadImg} src={uploadImg}></img>
+                            <img className={styles.formAddImg} src={addImg}></img>
+                        </div>
+                        <h5>Outras imagens</h5>
+                        <div className={styles.formCarregarImg}>
+                            <input type="text" className={styles.formCarregarImgTxt}/> 
+                            <img className={styles.formUploadImg} src={uploadImg}></img>
+                            <img className={styles.formAddImg} src={addImg}></img>
+                        </div>
+                    </div>
+                    <div className={styles.formCentralizaBtn}>
+                        <button className={styles.formBtn}>Criar Acomodação</button>
                     </div>
                 </div>
             </div>
