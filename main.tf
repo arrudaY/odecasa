83b44a571921737236f5fe3aa3d7647a53d72c3a
Thiago Cavalcante <xxtkbladexx@gmail.com>
2023-06-30 20:30:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,13 @@ data "aws_instances" "existing_instances" {
   }
 }
 
+resource "aws_codedeploy_app" "deploy1" {
+  name             = "g2-odecasa-dev"
+  compute_platform = "Server"
+}
+
 resource "aws_codedeploy_deployment_group" "deploygroup1" {
-  app_name               = "g2-odecasa-dev"
+  app_name               = aws_codedeploy_app.deploy1.name
   deployment_group_name  = "back-prod"
   deployment_config_name = "CodeDeployDefault.AllAtOnce"
   service_role_arn       = "arn:aws:iam::405378853534:role/GP2-User"
