c988e5f05068332ba19c134bdaf5c9f6eb99986f
Thiago Cavalcante <xxtkbladexx@gmail.com>
2023-06-30 00:44:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,14 +20,28 @@ resource "aws_codedeploy_deployment_group" "deploygroup1" {
   service_role_arn       = "arn:aws:iam::405378853534:role/GP2-User"
 
   ec2_tag_set {
-    tag_filter_type = "KEY_AND_VALUE"
+    ec2_tag_filter {
+      key   = "tag_key_1"
+      value = "DEPLOY"
+      type  = "KEY_AND_VALUE"
+    }
 
-    tags = {
-      "tag_key_1" = "DEPLOY"
-      "tag_key_2" = "GP2"
-      "tag_key_3" = "DEV"
-      "tag_key_4" = "BACK"
-      // Add more tags as needed
+    ec2_tag_filter {
+      key   = "tag_key_2"
+      value = "GP2"
+      type  = "KEY_AND_VALUE"
+    }
+
+    ec2_tag_filter {
+      key   = "tag_key_3"
+      value = "DEV"
+      type  = "KEY_AND_VALUE"
+    }
+
+    ec2_tag_filter {
+      key   = "tag_key_4"
+      value = "BACK"
+      type  = "KEY_AND_VALUE"
     }
   }
 }
