ae261664ed537b5d1d5298db368994b19c2e9f12
Thiago Cavalcante <xxtkbladexx@gmail.com>
2023-07-01 01:01:37 +0000

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -16,3 +16,26 @@ sudo service docker start
 # Instalação do Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Criação do docker-compose.yml
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  tomcat:
+    build: .
+    ports:
+      - 8080:8080
+EOF
+
+# Criação do Dockerfile
+cat <<EOF > Dockerfile
+FROM tomcat:jdk17
+COPY ./airBNB-2.0.jar /usr/local/tomcat/webapps/airBNB-2.0.jar
+CMD ["catalina.sh", "run"]
+EOF
+
+# Build da imagem Docker
+docker build -t app:latest .
+
+# Executar o contêiner Docker
+docker run -d -p 8080:8080 app:latest java -jar /usr/local/tomcat/webapps/airBNB-2.0.jar
