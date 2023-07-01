b8f80f9b7ead0af6ec244cb86467873454196252
Thiago Cavalcante <xxtkbladexx@gmail.com>
2023-07-01 01:20:50 +0000

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Instala zip
-sudo yum install -y zip
+yum install -y zip
 
 #Copia arquivo
 aws s3 cp s3://app-gp2-s3/my-application.zip ./my-application.zip
@@ -10,8 +10,8 @@ aws s3 cp s3://app-gp2-s3/my-application.zip ./my-application.zip
 unzip my-application.zip
 
 # Instalação do Docker
-sudo yum install -y docker
-sudo service docker start
+yum install -y docker
+service docker start
 
 # Instalação do Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
