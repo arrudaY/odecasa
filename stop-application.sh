71a185141d5a79c9765e4fd2d160b698ff2b5051
Thiago Cavalcante <xxtkbladexx@gmail.com>
2023-07-02 19:35:49 +0000

    Update stop-application.sh

--- stop-application.sh
+++ stop-application.sh
@@ -4,13 +4,21 @@
 sudo usermod -aG docker $USER
 
 # Interrompe todos os containers Docker em execução
-docker ps -aq && docker stop $(docker ps -aq)
+containers=$(docker ps -aq)
+if [ -n "$containers" ]; then
+    docker stop $containers
+fi
 
 # Remove todos os containers Docker em execução
-docker ps -aq && docker rm $(docker ps -aq)
+if [ -n "$containers" ]; then
+    docker rm $containers
+fi
 
 # Remove todas as imagens Docker
-docker images -aq && docker rmi $(docker images -aq)
+images=$(docker images -aq)
+if [ -n "$images" ]; then
+    docker rmi $images
+fi
 
 # Remove versão antiga
 rm airBNB-2.0.jar
