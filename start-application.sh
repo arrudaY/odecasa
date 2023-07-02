5a818e2ff357eb340c8ffa38810f1a98dc7e0060
Thiago Cavalcante <xxtkbladexx@gmail.com>
2023-07-02 14:24:38 +0000

    Update start-application.sh

--- start-application.sh
+++ start-application.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 #login
+sudo service docker start
 sudo usermod -aG docker $USER
-sudo groupadd docker
-sudo service docker restart
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Build da imagem Docker
-docker build -t app:latest .
 
 # Executar o contêiner Docker
-docker run -d -p 8080:8080 app:latest java -jar /usr/local/tomcat/webapps/airBNB-2.0.jar
+docker run -d -p 8080:8080 app:latest
