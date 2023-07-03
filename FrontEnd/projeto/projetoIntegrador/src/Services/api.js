85d21a60e2831924c28ad49990a039072c1498ae
Paula Brandão Furlan <furlan.paulabrandao@gmail.com>
2023-07-03 18:24:26 -0300

    Ajeitando a url do banco

--- FrontEnd/projeto/projetoIntegrador/src/Services/api.js
+++ FrontEnd/projeto/projetoIntegrador/src/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: "http://ec2-54-226-11-155.compute-1.amazonaws.com:8080",
-    baseURL: "http://ec2-54-207-114-79.sa-east-1.compute.amazonaws.com:8080",
+    baseURL: "http://ec2-52-67-228-185.sa-east-1.compute.amazonaws.com:8080/",
     //baseURL: "http://localhost:8080",
 });
 
