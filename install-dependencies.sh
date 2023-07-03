8dda80b3cc2a6a7348c362c25432f7a8a7e591d8
Thiago Cavalcante <xxtkbladexx@gmail.com>
2023-07-03 16:15:37 +0000

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,23 +3,27 @@
 # Instala zip
 sudo yum install -y zip
 
+# Instala o OpenJDK 17 (ou outra versão desejada do Java)
+sudo amazon-linux-extras enable corretto8
+sudo yum install -y java-1.8.0-amazon-corretto-devel
+
+# Instala o Tomcat
+sudo yum install -y tomcat
+
+# Para o serviço do Tomcat (caso esteja em execução)
+sudo service tomcat stop
+
 # Copia arquivo
 aws s3 cp s3://app-gp2-s3/my-application.zip ./my-application.zip
 
 # Extrair o arquivo .zip
 unzip -o my-application.zip
 
-# Instalação do Docker
-sudo yum install -y docker
+# Remove o conteúdo da pasta webapps (opcional, se desejar limpar a implantação anterior)
+sudo rm -rf /usr/share/tomcat/webapps/*
 
-# Inicia o serviço docker
-sudo service docker start
+# Copia o arquivo .jar para a pasta webapps
+sudo cp airBNB-2.0.jar /usr/share/tomcat/webapps/
 
-# Construção da imagem Docker
-sudo docker build -t app:latest .
-
-sudo docker run -d -p 8080:8080 app:latest java -jar /usr/local/tomcat/webapps/airBNB-2.0.jar
-
-# Instalação do Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# Inicia o aplicativo .jar usando o comando java -jar
+sudo java -jar /usr/share/tomcat/webapps/airBNB-2.0.jar
